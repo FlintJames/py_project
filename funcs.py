3fc848bbe1b736a6db10c1bc7f3d9d14fc503b24
FlintJames <lyuciana@yandex.ru>
2024-01-17 01:12:30 +0300

    change

--- funcs.py
+++ funcs.py
@@ -7,3 +7,14 @@ num2 = int(input())
 sum_of_numbers = add(num1, num2)
 
 print(sum_of_numbers)
+
+
+def add(a, b):
+    return a - b
+
+num1 = int(input())
+num2 = int(input())
+
+sum_of_numbers = add(num1, num2)
+
+print(sum_of_numbers)
